85c6da5ebfdf0712621d9610ef6e869664f540c3
Tuhin Reza <tuhinreza02042002@gmail.com>
2026-02-02 11:06:58 +0600

    Leap year check

--- JavaScript/string and object/Summary/summary.js
+++ JavaScript/string and object/Summary/summary.js
@@ -1,5 +1,22 @@
-let arr =[10,20,30];
-console.log(arr);
-for(let x of arr){
-    console.log(x);
+// inch to fit
+// 12 inch 1 feet
+// function inchToFeet(value){
+//         let feet = value/12;
+//         let valu1 =Math.trunc(feet);
+//         let inch =(value%12);
+//         console.log(`Feet value is : ${valu1} and inch value is : ${inch}`);
+
+// }
+// inchToFeet(150);
+
+// LeapYear condition 
+// divisible by 4
+// divisileb bye 100 and 400
+
+function leapYear(Year)
+{
+    if( (Year%400==0 && Year%100==0) || (Year%4==0 && Year%100!=0 ) )return true;
+    else return false;
 }
+ let leapyearCheck=leapYear(2000);
+ console.log(leapyearCheck);
