80a06328608901c477be74438db54d6dcd068ea4
Tuhin Reza <tuhinreza02042002@gmail.com>
2026-02-19 04:32:19 +0600

    New project added

--- JavaScript/js-DOm/event/event.js
+++ JavaScript/js-DOm/event/event.js
@@ -8,17 +8,23 @@
 // // document.getElementById("login").addEventListener("click", () => {
 // //   document.getElementById("user").innerText = "New user Added";
 // });
-const form = document.querySelector("form");
-document.getElementById("btn-post-cmt").addEventListener("click", (event) => {
-    console.log('Hi im here');
-     const commentWrite = document.getElementById("text-area");
-     const comentBox = document.getElementById("commnet-sction");
-     console.log(commentWrite.value);
-     const newComment= document.createElement('p');
-     newComment.className="Comment";
-     newComment.innerText=commentWrite.value;
-     comentBox.appendChild(newComment);
-     form.reset();
-     form.reset();
-     
-});
+// const form = document.querySelector("form");
+// document.getElementById("btn-post-cmt").addEventListener("click", (event) => {
+//     console.log('Hi im here');
+//      const commentWrite = document.getElementById("text-area");
+//      const comentBox = document.getElementById("commnet-sction");
+//      console.log(commentWrite.value);
+//      const newComment= document.createElement('p');
+//      newComment.className="Comment";
+//      newComment.innerText=commentWrite.value;
+//      comentBox.appendChild(newComment);
+//      form.reset();
+//      form.reset();
+
+// });
+
+// document.getElementById("input-delete").addEventListener("keyup", (event) => {
+//   const write = document.getElementById("input-delete").value;
+//   document.getElementById("text-delete").disabled =
+//     write === "Delete" ? false : true;
+// });
